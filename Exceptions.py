47d23e17229d13a9f5d6d0b1abe31b971bbf8460
themkdemiiir <85563797+themkdemiiir@users.noreply.github.com>
2022-09-12 20:34:04 +0300

    Delete Exceptions.py

--- Exceptions.py
+++
@@ -1,6 +0,0 @@
-class InstanceNotFoundException(Exception):
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return f'InstanceNotFoundException: {self.message}'
